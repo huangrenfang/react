7d1b1c4ede0944ec46a184d1052352679836c2ca
huangrenfang <1263168236@qq.com>
2019-10-08 21:51:54 +0800

    modified:   first-app/src/App.js 	new file:   first-app/src/components/Class1.js 	new file:   first-app/src/components/Class2.js 	modified:   first-app/src/index.js 	new file:   test/dialog.html

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import Class1 from './components/Class1';
+import Class2 from './components/Class2';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        
+        
+        <Link to="/">class1</Link>
+        <Link to="/class2">class2</Link>
+        <Route path="/" component={Class1}/>
+        <Route path="/class2" component={Class2}/>
+      </div>
+    </Router>
   );
 }
 
--- first-app/src/index.js
+++ first-app/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import ClickCount from './ClickCounter';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<ClickCount />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
